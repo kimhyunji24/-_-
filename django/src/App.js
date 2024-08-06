57f4c9963400e69c2c74a7fc89b191f59afb9750
hyunji <hyunji4560@gmail.com>
2024-08-07 04:50:31 +0900

    Update Search

--- django/src/App.js
+++ django/src/App.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+
+import React , { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import List from './pages/List';
 import Footer from './components/Footer';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   const [favorites, setFavorites] = useState([]); // 상태 정의
 
   return (
     <Router>
+      <Header />
       <div>
-        <Header />
         <Routes>
-          {/* <Route path="/" element={<Home />} />  */}
-          <Route path="/search" element={<Home favorites={favorites} setFavorites={setFavorites} />} />
+          <Route path="/" element={<Home favorites={favorites} setFavorites={setFavorites} />} />
           <Route path="/favorites" element={<List favorites={favorites} />} />
+          <Route path="/search" element={<SearchResults favorites={favorites} setFavorites={setFavorites} />} />
         </Routes>
         <Footer />
       </div>
--- django/src/pages/SearchResults.js
+++ django/src/pages/SearchResults.js
@@ -1,43 +1,46 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import res from '../response.json'; // JSON 파일 임포트
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Modal from 'react-modal';
-import '../styles/gallery.css';
+import res from '../response.json'; // JSON 파일 임포트
 import emptyHeart from '../assets/empty-heart.png'; // 빈 하트 아이콘 경로
 import filledHeart from '../assets/filled-heart.png'; // 채워진 하트 아이콘 경로
+import '../styles/gallery.css';
 
 Modal.setAppElement('#root'); // Modal root 설정
 
-function Home({ favorites, setFavorites }) {
-  const [searchTerm, setSearchTerm] = useState('');
+function SearchResults({ favorites, setFavorites }) {
+  const location = useLocation();
+  const searchTerm = new URLSearchParams(location.search).get('query');
   const [searchResults, setSearchResults] = useState([]);
   const [similarResults, setSimilarResults] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const [hasSearched, setHasSearched] = useState(false); // 검색 상태 추가
 
-  const handleSearch = () => {
-    setHasSearched(true); // 검색 수행 상태 업데이트
+  useEffect(() => {
+    if (searchTerm) {
+      setHasSearched(true); // 검색 수행 상태 업데이트
 
-    const lowerCaseSearchTerm = searchTerm.toLowerCase();
-    const results = res.results.filter(item =>
-      item.product_name.toLowerCase() === lowerCaseSearchTerm
-    );
-    setSearchResults(results);
-
-    if (results.length > 0) {
-      setSelectedItem(results[0]);
-      setModalIsOpen(true);
-      setSimilarResults([]);
-    } else {
-      setModalIsOpen(false);
-      setSelectedItem(null);
-      const similar = res.results.filter(item =>
-        item.product_name.toLowerCase().includes(lowerCaseSearchTerm)
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+      const results = res.results.filter(item =>
+        item.product_name.toLowerCase() === lowerCaseSearchTerm
       );
-      setSimilarResults(similar);
+      setSearchResults(results);
+
+      if (results.length > 0) {
+        setSelectedItem(results[0]);
+        setModalIsOpen(true);
+        setSimilarResults([]);
+      } else {
+        setModalIsOpen(false);
+        setSelectedItem(null);
+        const similar = res.results.filter(item =>
+          item.product_name.toLowerCase().includes(lowerCaseSearchTerm)
+        );
+        setSimilarResults(similar);
+      }
     }
-  };
+  }, [searchTerm]);
 
   const openModalWithItem = (item) => {
     setSelectedItem(item);
@@ -61,16 +64,6 @@ function Home({ favorites, setFavorites }) {
 
   return (
     <div>
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search for any food"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-
       {selectedItem && (
         <Modal
           isOpen={modalIsOpen}
@@ -119,10 +112,8 @@ function Home({ favorites, setFavorites }) {
           </div>
         </div>
       )}
-
-      <Link to="/favorites">좋아요 목록 보기</Link>
     </div>
   );
 }
 
-export default Home;
+export default SearchResults;
